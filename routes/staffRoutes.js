757619a99be65a68c412111a12884997d9b89fca
makhalamatsoso <makhalamatsoso3@gmail.com>
2024-12-07 00:41:56 +0000

    added routes folder

---
+++ routes/staffRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const Staff = require('../models/staff');  // Import Staff model
+const router = express.Router();
+
+// Define CRUD operations for staff (similar to vehicleRoutes)
+
+module.exports = router;
